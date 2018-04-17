ae752a455fb3dc58202fd71cd05a6e82b3c907fa
Ehsan <36004865+EndLessGod@users.noreply.github.com>
2018-04-17 16:42:22 +0430

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,25 @@
+do
+local _ = {
+		SUDO = 495059735, ---ایدی سودو
+		sudoidforpanel = 495059735, ----ایدی سودو
+		cliidforpanel = 495059735,
+        SUDO_ID = {495059735,508943165,581072915}, --- ایدی سودو-Api-CLI
+		sudoapibot = {495059735,581072915},  ---ایدی سودووcli
+        Full_Sudo = {495059735,508943165,581072915}, ----ایدی سودو-Api-CLI
+        ChannelLogs= '@source_search', ----یوزرکانال لاگ
+		token = '508943165:AAEV7o3uuZS1W9iXD1WmR0Ykjki2bN5NyRA', -----توکن ربات API
+        TokenApibot = '596582954:AAGUfqLkOrbhQtB9K87UlGOeT1qbSYOtlbo', ---- توکن ربات API مکمل
+        ChannelUsername = '@source_search', ----یورر کانال
+        Channelforcejoin = '@source_search', ----یوزر کانال
+	    Sudousernameapi = 'minuteman', ---یوزرسودو
+        Channelusernameapi = 'source_search', ----یوزر کانال
+        Botusernameapi = 'apisupporter_bot', ----یوزر باتApi
+		salechannelbot = 'minuteman', --- یوزر سودو
+        Pardakht = 'https://t.me/joinchat/HYI_Nkblcx4RbNduLfAm2w', ---- لینک پرداختی
+        Botusernamelink = 'apisupporter_bot', ---یوزر ربات Api
+        Sendpayiduser = 508943165,   ----id api
+        apipanelbotuserid = 508943165, ---id api
+        Channelnameauto = 'ربات مدیریت گروه', ---name bot
+}
+return _
+end
